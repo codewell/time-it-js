69bb271c911fa50bb52927ac60f02fff1def7ca8
Filip Johansson <filip.johansson@protonmail.com>
2020-04-17 11:13:29 +0200

    Remove comment

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 // Function that times the execution of another funciton
 // Usage timeIt(foo, "Description of execution")(arg1, arg2, ...)
-
 const timeIt = (func, description) => async (...args) => {
   console.time(description);
   try {
-    return await func(...args); // Wait for the result here
+    return await func(...args);
   } catch (error) {
     throw error;
   } finally {
