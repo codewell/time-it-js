bc51ce4bf83d513e3be586e36f4c2a934573fd9b
Filip Johansson <filip.johansson@protonmail.com>
2020-04-17 10:50:25 +0200

    Add core func and tests

---
+++ tests/index.test.js
@@ -0,0 +1,69 @@
+const timeItDev = require("../lib/dev.js");
+const timeItProd = require("../lib/prod");
+
+const syncFunc = (val) => val + 1;
+
+const asyncFunc = (val) =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(syncFunc(val));
+    }, 1000);
+  });
+
+const brokenFunc = () =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(reject(new Error(" :( ")));
+    }, 1000);
+  });
+
+//
+// Test the sync func
+//
+// Test the sync func
+test("Synchronous function (dev):", () => {
+  return timeItDev(
+    syncFunc,
+    "Test 1"
+  )(1).then((result) => {
+    expect(result).toBe(2);
+  });
+});
+
+test("Synchronous function (dev):", () => {
+  return timeItProd(
+    syncFunc,
+    "Test 2"
+  )(1).then((result) => {
+    expect(result).toBe(2);
+  });
+});
+
+//
+// Async functions
+//
+
+test("Asynchronous function (dev):", () => {
+  return timeItDev(
+    asyncFunc,
+    "Test 3"
+  )(1).then((result) => {
+    expect(result).toBe(2);
+  });
+});
+
+test("Asynchronous function (dev):", () => {
+  return timeItProd(
+    asyncFunc,
+    "Test 4"
+  )(1).then((result) => {
+    expect(result).toBe(2);
+  });
+});
+
+//
+// Failures
+//
+test("Throw error", async () => {
+  await expect(timeItProd(brokenFunc, "Test 5")()).rejects.toThrow();
+});
